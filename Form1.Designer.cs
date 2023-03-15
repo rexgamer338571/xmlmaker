953aa176b01c50b163730d6581801e1362ade31f
rexgamer338571 <85365081+rexgamer338571@users.noreply.github.com>
2023-03-15 21:11:33 +0100

    Added source

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿using XMLMaker;
+
+
+namespace XMLMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(122, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 35);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(16, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cena";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(16, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 46);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kolor";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(24, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 56);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(24, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 56);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Nowy plik";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F);
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Location = new System.Drawing.Point(122, 74);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 35);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F);
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(122, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 35);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(16, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 46);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Ilość";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F);
+            this.textBox5.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox5.Location = new System.Drawing.Point(122, 155);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(121, 35);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(122, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 35);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F);
+            this.textBox6.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox6.Location = new System.Drawing.Point(122, 196);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(121, 35);
+            this.textBox6.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(16, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 46);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Stan";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F);
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(24, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(219, 56);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Ustawienia (WIP)";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(410, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "XMLMaker 4.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+using XMLMaker;
+
+namespace XMLMaker
+{
+    public partial class Form1 : Form
+    {
+        public static bool hasStarted;
+        public static string fileLoc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string xmlString = "<ITEM><ITEMTYPE>P</ITEMTYPE><ITEMID>" + this.textBox1.Text + "</ITEMID><COLOR>" + this.textBox2.Text + "</COLOR><QTY>" + this.textBox5.Text + "</QTY><PRICE>" + this.textBox4.Text + "</PRICE><CONDITION>" + this.textBox6.Text + "</CONDITION></ITEM>";
+
+            if (hasStarted)
+            {
+                using (StreamWriter writer = new StreamWriter(fileLoc, true))
+                {
+                    writer.WriteLine(xmlString);
+                }
+            }
+
+            button1.Text = "Dodano!";
+            button1.BackColor = Color.White;
+            button1.Enabled = false;
+            Task.Delay(5000).ContinueWith(_ =>
+            {
+                button1.Invoke(new Action(() =>
+                {
+                    button1.Enabled = true;
+                    button1.Text = "Dodaj";
+                    button1.BackColor = Color.Black;
+                }));
+            });
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            dialog.Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*";
+            dialog.Title = "Zapisz plik XMLM";
+            DialogResult result = dialog.ShowDialog();
+            
+            if (result == DialogResult.OK)
+            {
+                fileLoc = dialog.FileName;
+                using (StreamWriter writer = new StreamWriter(fileLoc, true))
+                {
+                    writer.WriteLine("<INVENTORY>");
+                }
+                hasStarted = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+
+            this.Size = new Size(500, 489);
+
+            FontConverter fontConverter = new FontConverter();
+            Font font = (Font)fontConverter.ConvertFromString("Microsoft YaHei UI, 26.25pt");
+            Font textBoxFont = (Font)fontConverter.ConvertFromString("Microsoft YaHei UI, 20.25pt");
+
+            #region Labels
+
+            Label settingLabel = new Label();
+            settingLabel.Text = "Ustawienia (WIP)";
+            settingLabel.Location = new Point(16, 16);
+            settingLabel.Size = new Size(500, 46);
+            this.Controls.Add(settingLabel);
+
+            Label fontColorLabel = new Label();
+            fontColorLabel.Text = "Kolor czcionki";
+            fontColorLabel.Location = new Point(16, 65);
+            fontColorLabel.Size = new Size(252, 46);
+            this.Controls.Add(fontColorLabel);
+
+            Label inputBackColorLabel = new Label();
+            inputBackColorLabel.Text = "Kolor wejścia";
+            inputBackColorLabel.Location = new Point(16, 106);
+            inputBackColorLabel.Size = new Size(237, 46);
+            this.Controls.Add(inputBackColorLabel);
+
+            Label backColorLabel = new Label();
+            backColorLabel.Text = "Kolor tła";
+            backColorLabel.Location = new Point(16, 147);
+            backColorLabel.Size = new Size(159, 46);
+            this.Controls.Add(backColorLabel);
+
+            #endregion Labels
+
+            #region Inputs
+
+            TextBox fontColorInput = new TextBox();
+            fontColorInput.Location = new Point(272, 65);
+            this.Controls.Add(fontColorInput);
+
+            TextBox inputBackColorInput = new TextBox();
+            inputBackColorInput.Location = new Point(272, 106);
+            this.Controls.Add(inputBackColorInput);
+
+            
+            TextBox backColorInput = new TextBox();
+            backColorInput.Location = new Point(272, 147);
+            this.Controls.Add(backColorInput);
+
+            #endregion Inputs
+
+            #region Buttons
+
+            Button saveSettings = new Button();
+            saveSettings.Text = "Zapisz ustawienia";
+            saveSettings.Location = new Point(26, 208);
+            this.Controls.Add(saveSettings);
+
+            Button backToMain = new Button();
+            backToMain.Text = "Powrót";
+            backToMain.Location = new Point(26, 265);
+            backToMain.Click += new EventHandler(backToMain_Click);
+            this.Controls.Add(backToMain);
+
+            #endregion Buttons
+
+            foreach (Control control in this.Controls)
+            {
+                control.ForeColor = Color.White;
+                control.Font = font;
+
+                string type = control.GetType().Name;
+
+                if (type == "TextBox") 
+                {
+                    control.ForeColor = Color.Black;
+                    control.Font = textBoxFont;
+                    control.Size = new Size(200, 46);
+                } else if (type == "Button") 
+                {
+                    control.Size = new Size(448, 57);
+                }
+            }
+        }
+        private void backToMain_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            InitializeComponent();
+        }
+
+        /*
+        private void saveSettings_Click(object sender, EventArgs e)
+        {
+            string settingsLoc = "C:\\NG5M\\XMLMaker4\\0\\settings";
+
+            if (!File.Exists(settingsLoc + "\\labelFontColor.xmlms"))
+            {
+                using(FileStream fs = File.Create(settingsLoc + "\\labelFontColor.xmlms"))
+                {
+                    using(StreamWriter writer = new StreamWriter(settingsLoc + "\\labelFontColor.xmlms"))
+                    {
+                        writer.WriteLine(this.Controls.Find("fontColorInput", true));
+                    }
+                }
+            }
+        }
+        
+        private void LoadBLImg(string number)
+        {
+            var client = new RestClient("https://api.bricklink.com");
+            var request = new RestRequest("items/{type}/{no}/images/{color_id}", Method.Get);
+            request.AddUrlSegment("type", "P");
+            request.AddUrlSegment("number", number);
+            request.AddHeader("Authorization", "Bearer 7CCDCEF257CF43D89A74A7E39BEAA1E1");
+            var response = client.Execute(request);
+
+            var jsonResponse = JObject.Parse(response.Content);
+            var imgURL = jsonResponse["data"]["item"]["images"][0]["thumbnail_url"];
+
+            pictureBox1.Load(imgURL.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            LoadBLImg("3003");
+        }
+        
+        private void applySettings(object sender, EventArgs e)
+        {
+            backColor = ColorTranslator.FromHtml(backColorInput.Text);
+            BackColor = backColor;
+
+            fontColor = ColorTranslator.FromHtml(fontColorInput.Text);
+            ForeColor = fontColor;
+
+            inputColor = ColorTranslator.FromHtml(inputBackColorInput.Text);
+
+        }
+        */
+
+    }
+}
